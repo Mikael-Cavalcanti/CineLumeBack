83be1c42c5f71f5924df059285e4559efc94d028
mwsa2 <mwsa2@cin.ufpe.br>
2025-07-21 16:21:00 -0300

    profile first CRUD

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,8 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsString } from 'class-validator'
+import { IsString, IsBoolean } from 'class-validator'
 
 export class CreateProfileDto {
     @ApiProperty({ example: 'Alice' })
     @IsString()
     name: string
+
+    @ApiProperty({ example: 'Avatar url' })
+    @IsString()
+    avatar_url: string
+
+    @ApiProperty()
+    @IsBoolean()
+    is_kid_profile: boolean;
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 import { Profile } from '@prisma/client';
 
 @Injectable()
@@ -13,6 +14,8 @@ export class ProfileService {
         data: {
           userId: 1,
           name: dto.name,
+          avatarUrl: dto.avatar_url,
+          isKidsProfile: dto.is_kid_profile,
         },
       });
     } catch (error) {
@@ -31,4 +34,32 @@ export class ProfileService {
       throw new Error('Error fetching profiles');
     }
   }
+
+  async updateProfile(id: number, dto: UpdateProfileDto): Promise<Profile | null> {
+      try {
+        return await this.prisma.profile.update({
+          where: { id },
+          data: {
+            name: dto.name,
+            avatarUrl: dto.avatar_url,
+            isKidsProfile: dto.is_kid_profile,
+          },
+        });
+      } catch (e) {
+        console.error('Error updating profile:', e);
+        return null;
+      }
+    }
+
+  async removeProfile(id: number): Promise<Profile | null> {
+      try {
+        return await this.prisma.profile.delete({
+          where: { id },
+        });
+      } catch (err) {
+        console.error('Error removing profile:', err);
+        return null;
+      }
+    }
+
 }
