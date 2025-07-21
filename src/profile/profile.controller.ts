bd8d7137df6b563ced23ca166f038994b5cf896f
mwsa2 <mwsa2@cin.ufpe.br>
2025-07-20 23:57:17 -0300

    profile created

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, UseGuards } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { AssignProfileDto } from './dto/assign-profile.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
-@Controller('profile')
-export class ProfileController {}
+@ApiTags('Profiles')
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) { }
+
+  //@UseGuards(JwtAuthGuard)
+  //@ApiBearerAuth()
+  @Get()
+  async getAllProfiles() {
+    try {
+      const profiles = await this.profileService.getAllProfiles();
+      return { success: true, data: profiles };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+  
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Post()
+  async createProfile(@Body() body: CreateProfileDto) {
+    try {
+      const profile = await this.profileService.createProfile(body);
+      return { success: true, data: profile };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+}
