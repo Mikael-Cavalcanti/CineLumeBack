758ac516831b806f3b4836d576b10475698e1128
Mikael Cavalcanti <mikaelcavalcanti@outlook.com>
2025-07-22 23:29:21 -0300

    fix(user test): add more scenarios

--- src/user/test/user.feature
+++ src/user/test/user.feature
@@ -10,3 +10,13 @@ Feature: Gerenciar usuários no sistema
     Given existe um usuário cadastrado com nome "Maria", email "Maria@example.com"
     When é feita uma busca por usuário com email "josé@example.com"
     Then deve retornar "null" para o usuário com email "josé@example.com"
+
+  Scenario: Atualizar um usuário com sucesso
+    Given que existe um usuário cadastrado com id "1", nome "João Silva", email "joao@example.com", senha "123456" e data de nascimento "2000-01-01"
+    When o usuário com id "1" é atualizado para "João da Silva" com email "joao@example.com", senha "123456" e data de nascimento "2000-01-01"
+    Then o usuário é atualizado no sistema com nome "João da Silva", email "joao@example.com", senha "123456" e data de nascimento "2000-01-01"
+
+  Scenario: Remover um usuário com sucesso
+    Given que existe um usuário cadastrado com id "1", nome "João Silva", email "joao@example.com", senha "123456" e data de nascimento "2000-01-01"
+    When o usuário com id "1" é removido do sistema
+    Then o usuário com id "1" deve retornar "null" ao ser buscado
--- src/user/test/user.steps.spec.ts
+++ src/user/test/user.steps.spec.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { UsersService } from '../../user/users.service';
 import { CreateUserDto } from '../../user/dto/create-user.dto';
 import { User } from '@prisma/client';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 const feature = loadFeature('src/user/test/user.feature');
 
@@ -16,6 +17,8 @@ defineFeature(feature, (test) => {
       user: {
         findUnique: jest.fn(),
         create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
       },
     } as unknown as PrismaService;
 
@@ -23,7 +26,7 @@ defineFeature(feature, (test) => {
     jest.clearAllMocks();
   });
 
-  //Cenário: Criar um usuário com sucesso
+  //Cenário 1: Criar um usuário com sucesso
   test('Criar um usuário com sucesso', ({ given, when, then }) => {
     given(
       /^que não existe um usuário cadastrado com nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
@@ -82,7 +85,7 @@ defineFeature(feature, (test) => {
     });
   });
 
-  // Cenário: Buscar um usuário por ID com sucesso
+  // Cenário 2: Buscar um usuário por ID com sucesso
   test('Erro ao buscar um usuário com email inexistente', ({
     given,
     when,
@@ -115,6 +118,132 @@ defineFeature(feature, (test) => {
         expect(prisma.user.findUnique).toHaveBeenCalledWith({
           where: { email },
         });
+      },
+    );
+  });
+
+  // Cenário 3: Atualizar um usuário com sucesso
+  test('Atualizar um usuário com sucesso', ({ given, when, then }) => {
+    given(
+      /^que existe um usuário cadastrado com id "([^"]+)", nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
+      (
+        id: string,
+        name: string,
+        email: string,
+        password: string,
+        birthDate: string,
+      ) => {
+        (prisma.user.findUnique as jest.Mock).mockResolvedValue({
+          id: Number(id),
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        });
+
+        (prisma.user.update as jest.Mock).mockResolvedValue({
+          id: Number(id),
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        });
+      },
+    );
+
+    when(
+      /^o usuário com id "([^"]+)" é atualizado para "([^"]+)" com email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
+      async (
+        id: number,
+        name: string,
+        email: string,
+        password: string,
+        birthDate: Date,
+      ) => {
+        (prisma.user.update as jest.Mock).mockResolvedValue({
+          id: Number(id),
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        });
+
+        resultant = await usersService.update(Number(id), {
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        } as UpdateUserDto);
+      },
+    );
+
+    then(
+      /^o usuário é atualizado no sistema com nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
+      (name: string, email: string, password: string, birthDate: string) => {
+        expect(resultant).toEqual({
+          id: 1,
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        });
+
+        expect(prisma.user.update).toHaveBeenCalledWith({
+          where: { id: 1 },
+          data: {
+            name,
+            email,
+            password,
+            birthDate: new Date(birthDate),
+          },
+        });
+      },
+    );
+  });
+
+  // Cenário 4: Remover um usuário com sucesso
+  test('Remover um usuário com sucesso', ({ given, when, then }) => {
+    given(
+      /^que existe um usuário cadastrado com id "([^"]+)", nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
+      (
+        id: string,
+        name: string,
+        email: string,
+        password: string,
+        birthDate: string,
+      ) => {
+        (prisma.user.findUnique as jest.Mock).mockResolvedValue({
+          id: Number(id),
+          name,
+          email,
+          password,
+          birthDate: new Date(birthDate),
+        });
+      },
+    );
+
+    when(
+      /^o usuário com id "([^"]+)" é removido do sistema$/,
+      async (id: string) => {
+        (prisma.user.delete as jest.Mock).mockResolvedValue({
+          id: Number(id),
+          name: 'João Silva',
+          email: 'joao@example.com',
+          password: '123456',
+          birthDate: new Date('2000-01-01'),
+        });
+
+        await usersService.remove(Number(id));
+      },
+    );
+
+    then(
+      /^o usuário com id "([^"]+)" deve retornar "null" ao ser buscado$/,
+      async (id: string) => {
+        (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
+
+        const result = await usersService.findOne(Number(id));
+        expect(result).toBeNull();
       },
     );
   });
