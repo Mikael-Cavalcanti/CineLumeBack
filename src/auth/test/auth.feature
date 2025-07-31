94b929554680a3c616e2e6a1f4af8bffb04829e8
Mikael Cavalcanti <mikaelcavalcanti@outlook.com>
2025-07-31 00:56:41 -0300

    fix(authService_test): adjust test logic

--- src/auth/test/auth.feature
+++ src/auth/test/auth.feature
@@ -3,21 +3,24 @@ Feature: Cadastro e Login de usuários
 Feature: Cadastro e Login de usuários
 
   Scenario Outline: Cadastro de usuário com sucesso
-    Given que não existe um usuário cadastrado com email "<email>"
-    When um novo usuário é criado com nome "<nome>", email "<email>", senha "<senha>" e data de nascimento "<nascimento>"
-    And é criado um perfil de usuário com nome "<nome>", isKid "false" e avatar "<avatar>"
+    Given que não existe um usuário cadastrado
+    When um novo usuário é criado com id "<id>", nome "<nome>", email "<email>", senha "<senha>" e data de nascimento "<nascimento>"
+    And é criado um perfil do usuário "<id>" com nome "<nome>", isKid "false" e avatar "<avatar>"
     And é criado um token "<token>" para o usuário com id "<id>"
     Then o usuário é salvo no sistema com nome "<nome>", email "<email>"
     And o id do perfil tem que ser "<id>" do usuário
     And o usuário com id "<id>" recebe o token de autenticação "<token>"
     Examples:
-      | id | nome       | email            | senha     | nascimento | token       | avatar                     |
-      | 1  | João Silva | joao@example.com | Senha@123 | 2000-01-01 | token123456 | example.com.br/avatar1.png |
+      | id | nome        | email             | senha     | nascimento | token       | avatar                     |
+      | 1  | João Silva  | joao@example.com  | Senha@123 | 2000-01-01 | token123456 | example.com.br/avatar1.png |
+      | 2  | Maria Souza | maria@example.com | Senha@456 | 1995-05-15 | token654321 | example.com.br/avatar2.png |
 
 #  Scenario Outline: Login de usuário com sucesso
-#    Given que existe um usuário cadastrado com email "<email>" e senha "<senha>"
+#    Given que existe um usuário cadastrado com email "<email>" e senha "<senha>", id "<id>", nome "<nome>" e nascimento "<nascimento>"
+#    And o token "<token>" para o usuário com id "<id>" ainda não está expirado
 #    When o usuário tenta fazer login com email "<email>" e senha "<senha>"
-#    Then o login é bem-sucedido e o retorna o token de autenticação "<token>"
+#    Then o login é bem-sucedido e o retorna o token de autenticação para o usuário com id "<id>"
+#    And o usuário com id "<id>" recebe o token de autenticação "<token>"
 #    Examples:
-#      | email            | senha     | token       |
-#      | joao@example.com | Senha@123 | token123456 |
+#      | id | nome       | email            | senha     | nascimento | token       |
+#      | 1  | João Silva | joao@example.com | Senha@123 | 2000-01-01 | token123456 |
--- src/auth/test/auth.steps.ts
+++ src/auth/test/auth.steps.ts
@@ -1,21 +1,27 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import { PrismaService } from '../../prisma/prisma.service';
-import { Profile, ResetToken, User } from '@prisma/client';
 import { ProfileService } from '../../profile/profile.service';
 import { UsersService } from '../../user/users.service';
+import { AuthService } from '../auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { MailService } from '../../mail/mail.service';
 import { CreateUserDto } from '../../user/dto/create-user.dto';
+import { User } from '@prisma/client';
 import { CreateProfileDto } from '../../profile/dto/create-profile.dto';
+import { RegisterDto } from '../dto/register.dto';
 
 const feature = loadFeature('src/auth/test/auth.feature');
 
-let profileService: ProfileService;
+let authService: AuthService;
 let userService: UsersService;
+let profileService: ProfileService;
 
-let resultProfile: Profile;
+// Resultados
+let result: { accessToken: string };
+let resultProfile: { userId: number };
 let resultUser: User;
-let resultToken: ResetToken;
 
-// Mock do Prisma
+// Mocks
 const prisma = {
   user: {
     findUnique: jest.fn(),
@@ -24,16 +30,33 @@ const prisma = {
   },
   resetToken: {
     create: jest.fn(),
+    update: jest.fn(),
+    findFirst: jest.fn(),
   },
   profile: {
     create: jest.fn(),
   },
 } as unknown as PrismaService;
 
+const jwtService = {
+  sign: jest.fn(),
+} as unknown as JwtService;
+
+const mailService = {
+  sendEmailCode: jest.fn(),
+} as unknown as MailService;
+
 defineFeature(feature, (test) => {
   beforeAll(() => {
-    profileService = new ProfileService(prisma);
     userService = new UsersService(prisma);
+    profileService = new ProfileService(prisma);
+    authService = new AuthService(
+      userService,
+      jwtService,
+      mailService,
+      prisma,
+      profileService,
+    );
   });
 
   beforeEach(() => {
@@ -41,52 +64,53 @@ defineFeature(feature, (test) => {
   });
 
   test('Cadastro de usuário com sucesso', ({ given, when, then }) => {
-    given(/^que não existe um usuário cadastrado com email "([^"]+)"$/, () => {
+    given('que não existe um usuário cadastrado', () => {
       (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
     });
 
     when(
-      /^um novo usuário é criado com nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
+      /^um novo usuário é criado com id "([^"]+)", nome "([^"]+)", email "([^"]+)", senha "([^"]+)" e data de nascimento "([^"]+)"$/,
       async (
+        id: string,
         name: string,
         email: string,
         password: string,
-        birthDay: string,
+        birthDate: string,
       ) => {
         (prisma.user.create as jest.Mock).mockResolvedValue({
-          id: 1,
+          id: Number(id),
           name,
           email,
-          password,
-          birthDate: new Date(birthDay),
+          password: password,
+          birthDate: new Date(birthDate),
         });
 
+        (mailService.sendEmailCode as jest.Mock).mockResolvedValue(undefined);
+
         resultUser = await userService.create({
+          id: Number(id),
           name,
           email,
           password,
-          birthDate: new Date(birthDay),
+          birthDate: new Date(birthDate),
         } as CreateUserDto);
       },
     );
 
     when(
-      /^é criado um perfil de usuário com nome "([^"]+)", isKid "([^"]+)" e avatar "([^"]+)"$/,
-      async (name: string, isKid: string, avatar: string) => {
-        const isKidBool = isKid === 'true';
-
+      /^é criado um perfil do usuário "([^"]+)" com nome "([^"]+)", isKid "([^"]+)" e avatar "([^"]+)"$/,
+      async (id: number, name: string, isKid: boolean, avatar: string) => {
         (prisma.profile.create as jest.Mock).mockResolvedValue({
-          id: 1,
+          userId: Number(id),
           name,
-          isKidProfile: isKidBool,
+          isKidProfile: isKid,
           avatar,
-          userId: resultUser.id,
         });
 
         resultProfile = await profileService.createProfile({
-          userId: resultUser.id,
           name,
-          isKidProfile: isKidBool,
+          userId: Number(id),
+          isKidProfile: isKid,
           avatar,
         } as CreateProfileDto);
       },
@@ -95,47 +119,49 @@ defineFeature(feature, (test) => {
     when(
       /^é criado um token "([^"]+)" para o usuário com id "([^"]+)"$/,
       async (token: string, id: string) => {
-        const expiresAt = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
+        (jwtService.sign as jest.Mock).mockReturnValue(token);
 
+        const expiresAt = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
         (prisma.resetToken.create as jest.Mock).mockResolvedValue({
-          id: 1,
           userId: Number(id),
           token,
           expiresAt,
+          used: false,
         });
 
-        resultToken = await prisma.resetToken.create({
-          data: {
-            userId: Number(id),
-            token,
-            expiresAt,
-          },
+        (prisma.resetToken.update as jest.Mock).mockResolvedValue({
+          userId: Number(id),
+          token,
+          expiresAt,
+          used: true,
         });
+
+        result = await authService.register({
+          name: resultUser.name,
+          email: resultUser.email,
+          password: resultUser.password,
+          birthDate: resultUser.birthDate,
+        } as RegisterDto);
       },
     );
 
     then(
       /^o usuário é salvo no sistema com nome "([^"]+)", email "([^"]+)"$/,
       (name: string, email: string) => {
-        expect(resultUser).toEqual(
-          expect.objectContaining({
-            name,
-            email,
-          }),
-        );
+        expect(resultUser.name).toEqual(name);
+        expect(resultUser.email).toEqual(email);
       },
     );
 
     then(/^o id do perfil tem que ser "([^"]+)" do usuário$/, (id: string) => {
-      expect(resultProfile.userId).toBe(Number(id));
-      expect(resultProfile.userId).toBe(resultUser.id);
+      expect(resultProfile.userId).toEqual(Number(id));
     });
 
     then(
       /^o usuário com id "([^"]+)" recebe o token de autenticação "([^"]+)"$/,
-      (id: string, token: string) => {
-        expect(resultToken.userId).toBe(Number(id));
-        expect(resultToken.token).toBe(token);
+      (id: number, token: string) => {
+        expect(resultUser.id).toEqual(Number(id));
+        expect(result.accessToken).toEqual(token);
       },
     );
   });
